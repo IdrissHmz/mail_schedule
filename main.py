88d1c769fa02729dd5111f3d7d65506e5c1ebbe4
Idriss <hi_hamzaoui@esi.dz>
2022-12-21 15:43:02 +0100

    master template added

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def create_email_message(sender, receivers, subject, body):
 def send(sender, receiver, em):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
-        smtp.login(sender, pwd)
+        smtp.login(sender, APP_PASSWORD)
         smtp.sendmail(sender, receiver, em.as_string())
 
 
@@ -32,14 +32,18 @@ def add_attachment(pdf, em):
 def create_body(args, type):
     body = ''
     match type :
-        case 'stage':
-            with open("html_formatted_emails/example.html", "rb") as html:
+        case 'stage_fr':
+            with open("html_formatted_emails/stage_fr.html", "rb") as html:
                 body = html.read().decode("utf-8")
                 body = body.format(laboratoire=args['laboratoire'], github=args['github']).replace(
                     "\n", ""
                 )
-        case 'master':
-            print('master')    
+        case 'master_en':
+            with open("html_formatted_emails/master_en.html", "rb") as html:
+                body = html.read().decode("utf-8")
+                body = body.replace(
+                    "\n", ""
+                )
         case 'job':
             print('job')        
         case _:
@@ -47,17 +51,17 @@ def create_body(args, type):
     return body  
 
 sender = "hi_hamzaoui@esi.dz"
-pwd = "fnuaouprnyozevwc"
-type = 'stage'
+APP_PASSWORD = "fnuaouprnyozevwc"
+type = 'stage_fr'
 
  
 conf = json.load(open('conf.json'))
-   
-var = conf['stage']
+var = conf[type]
 sender = var['sender']
 receivers = var['receivers']
 subject = var['subject']
 args_body = var['args_body']
+
 
 body = create_body(args_body, type)
 em = create_email_message(sender, receivers, subject, body)
